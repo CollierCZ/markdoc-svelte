bc01c192d713b6ec0f9af4512192cfb9faf203c5
Aaron Collier <aaron@collier.cz>
2022-05-25 20:58:27 +0200

    📦️ Limit files for published package

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,23 @@
-import dts from 'rollup-plugin-dts';
+import dts from "rollup-plugin-dts";
 import { terser } from "rollup-plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 
 export default [
   {
-  input: "src/main.ts",
-  output: [
-    { file: "dist/main.cjs.js", format: "cjs", sourcemap: true },
-    { file: "dist/main.es.js", format: "es", sourcemap: true },
-  ],
-  external: ["js-yaml", "@markdoc/markdoc", "markdown-it"],
-  plugins: [terser(), typescript()],
+    input: "src/main.ts",
+    output: [
+      { file: "dist/main.cjs.js", format: "cjs", sourcemap: true },
+      { file: "dist/main.es.js", format: "es", sourcemap: true },
+    ],
+    external: ["js-yaml", "@markdoc/markdoc", "markdown-it"],
+    plugins: [terser(), typescript()],
   },
-	{
-		input: 'src/main.ts',
-		output: [
-			{ file: 'dist/main.es.d.ts', format: 'es' },
-			{ file: 'dist/main.cjs.d.ts', format: 'cjs' },
-		],
-    plugins: [dts()]
-	}
-]
+  {
+    input: "src/main.ts",
+    output: [
+      { file: "dist/main.es.d.ts", format: "es" },
+      { file: "dist/main.cjs.d.ts", format: "cjs" },
+    ],
+    plugins: [dts()],
+  },
+];
