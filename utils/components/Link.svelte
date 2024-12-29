11571cc4a118d51afad96f609689e32b848f471d
Aaron Collier <aaron@collier.cz>
2024-12-29 13:02:23 +0100

    ✅ Add test for Svelte component node

---
+++ utils/components/Link.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  let {
+    children,
+    class = "",
+    rel = "",
+    href,
+  }: { children: any; class: string, href: string; rel: string } = $props();
+</script>
+
+<a {href} rel={rel ? "noopener" : ""} class={`${class} otherClass`}>
+  {@render children?.()}
+</a>
---
+++ utils/schema/nodes/link.ts
@@ -0,0 +1,32 @@
+import { Tag } from "@markdoc/markdoc";
+import type { Config, Node } from "@markdoc/markdoc";
+
+export const link = {
+  render: "Link",
+  children: ['inline'],
+  attributes: {
+    href: {
+      type: String
+    },
+    title: {
+      type: String
+    }
+  },
+  transform(node: Node, config: Config) {
+    const attributes = node.transformAttributes(config);
+    const children = node.transformChildren(config);
+
+    console.log("hi")
+
+    const relValue = attributes.href.startsWith("http") ? "noopener" : ""
+    const classValue = attributes.class ? `${attributes.class} font-semibold` : "font-semibold"
+
+    return new Tag(
+      this.render,
+      { ...attributes, rel: relValue, class: classValue },
+      children
+    );
+
+  }
+};
+
