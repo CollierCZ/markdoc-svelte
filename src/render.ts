e6ac2713adb8e145fe7228422c2350f4e47e2788
Aaron Collier <aaron@collier.cz>
2025-01-08 18:16:59 +0100

    ✨ Export escape functions

--- src/render.ts
+++ src/render.ts
@@ -4,6 +4,7 @@ import type {
   Scalar,
 } from "@markdoc/markdoc";
 import MarkdownIt from "markdown-it";
+import { escapeMarkdocBrackets } from "./utils";
 
 const { escapeHtml } = MarkdownIt().utils;
 
@@ -25,27 +26,6 @@ const voidElements = new Set([
   "wbr",
 ]);
 
-const BRACKET_ESCAPE_TEST_RE = /[{}]/;
-const BRACKET_ESCAPE_REPLACE_RE = /[{}]/g;
-const BRACKET_REPLACEMENTS = {
-  "{": "&lcub;",
-  "}": "&rcub;",
-};
-
-function replaceUnsafeChar(character: string) {
-  if (character === "{" || character === "}") {
-    return BRACKET_REPLACEMENTS[character];
-  }
-  return character;
-}
-
-function escapeCharacters(str: string) {
-  if (BRACKET_ESCAPE_TEST_RE.test(str)) {
-    return str.replace(BRACKET_ESCAPE_REPLACE_RE, replaceUnsafeChar);
-  }
-  return escapeHtml(str);
-}
-
 const render = (node: RenderableTreeNodes): string => {
   if (typeof node === "string") return node;
 
@@ -62,7 +42,7 @@ const render = (node: RenderableTreeNodes): string => {
     const attributesList = Object.entries(attributes ?? {}).reduce(
       (previous, [key, value]) =>
         `${previous} ${key}="${escapeHtml(String(value))}"`,
-      "",
+      ""
     );
     const openingTag = `<${name}${attributesList}`;
 
@@ -80,16 +60,16 @@ const render = (node: RenderableTreeNodes): string => {
           | {
               [key: string]: Scalar;
             }
-          | RenderableTreeNode[],
+          | RenderableTreeNode[]
       ): string => {
         if (typeof arrayOfChildren === "string")
-          return escapeCharacters(arrayOfChildren);
+          return escapeMarkdocBrackets(arrayOfChildren);
         if (Array.isArray(arrayOfChildren)) {
           return arrayOfChildren
             .map((child) =>
-              escapeCharacters(
-                child?.toString().replace("\n", "") || "", // Don't break the string in the middle // Make sure we end up with strings, not null or undefined
-              ),
+              escapeMarkdocBrackets(
+                child?.toString().replace("\n", "") || "" // Don't break the string in the middle // Make sure we end up with strings, not null or undefined
+              )
             )
             .join("\n"); // Put the line breaks back in
         }
