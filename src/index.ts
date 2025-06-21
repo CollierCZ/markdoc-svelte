edc69a708e5dbc237f37eeee60b8cdd2c480d559
Aaron Collier <aaron@collier.cz>
2025-06-21 20:31:07 +0200

    💥 Remove old name

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 export { markdocPreprocess } from "./main.ts";
-// The markdoc alias is maintained for backward compatibility.
-// It can be removed in a future major version.
-export { markdocPreprocess as markdoc } from "./main.ts";
 export type { MarkdocModule } from "./types.ts";
 
 export { default as Markdoc } from "@markdoc/markdoc";
