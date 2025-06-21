dcbc2ba9467273641e895c9d53be825d1fc9ea9b
Aaron Collier <aaron@collier.cz>
2025-06-21 18:54:14 +0200

    ♻️ Move options tests together

--- test/constants.ts
+++ test/constants.ts
@@ -293,6 +293,20 @@ keywords:
 Content. Such great content.
 `;
 
+export const markdocWithTypography = `${basicMarkdoc}
+## Typographic replacements
+
+Enable typographer option to see result.
+
+(c) (C) (r) (R) (tm) (TM) (p) (P) +-
+
+test.. test... test..... test?..... test!....
+
+!!!!!! ???? ,,  -- ---
+
+"Smartypants, double quotes" and 'single quotes'
+`;
+
 export const markdocWithComponent = `${basicMarkdoc}
 {% button text="Hello world" /%}
 `;
