bed08d2dce81c4cd82b91ae36bf109f16e6f6162
Aaron Collier <aaron@collier.cz>
2025-06-23 23:15:31 +0200

    🎨 Prettify

--- packages/docs/vitest.config.js
+++ packages/docs/vitest.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitest/config'
-import { sveltekit } from '@sveltejs/kit/vite'
-import { svelteTesting } from '@testing-library/svelte/vite'
+import { defineConfig } from "vitest/config";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { svelteTesting } from "@testing-library/svelte/vite";
 
 export default defineConfig(() => ({
   plugins: [sveltekit(), svelteTesting()],
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest-setup.js'],
+    environment: "jsdom",
+    setupFiles: ["./vitest-setup.js"],
     coverage: {
-      include: ['src/**'],
+      include: ["src/**"],
     },
   },
-}))
+}));
