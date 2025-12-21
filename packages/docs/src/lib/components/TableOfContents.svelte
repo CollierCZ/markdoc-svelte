363c4eb3a5c2102e6650b61e45938b1983f64a2c
Aaron Collier <aaron@collier.cz>
2025-12-21 15:11:08 +0100

    🚨 Add key to each block

--- packages/docs/src/lib/components/TableOfContents.svelte
+++ packages/docs/src/lib/components/TableOfContents.svelte
@@ -2,13 +2,13 @@
   import type { MarkdocModule } from "markdoc-svelte"
   const { headings }: MarkdocModule = $props();
 
-  // Filter only h1 and h2 headings
+  // Include only h1 and h2 headings
   const filteredHeadings = headings?.filter((heading) => heading.level <= 2) ?? [];
 </script>
 
 {#if filteredHeadings.length > 0}
   <ul>
-    {#each filteredHeadings as heading}
+    {#each filteredHeadings as heading (heading.id)}
       <li>
         <a href={`#${heading.id}`}>{heading.title}</a>
       </li>
