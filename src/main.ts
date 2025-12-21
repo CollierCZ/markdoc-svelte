6976a4cf81b69089d062d84317f5de775c4d8b3c
Aaron Collier <aaron@collier.cz>
2025-12-21 12:44:03 +0100

    🎨 Prettify

--- src/main.ts
+++ src/main.ts
@@ -46,7 +46,7 @@ export const markdocPreprocess = (options: Options = {}): PreprocessorGroup => {
   for (const key in options) {
     if (!validOptionKeys.includes(key as keyof Options)) {
       log.warn(
-        `Invalid option "${key}" provided and ignored. Check the documentation for valid options.`
+        `Invalid option "${key}" provided and ignored. Check the documentation for valid options.`,
       );
     }
   }
@@ -212,7 +212,7 @@ export const markdocPreprocess = (options: Options = {}): PreprocessorGroup => {
         extractUsedSvelteComponents(transformedContent);
       const componentImportStatements = getComponentImports(
         usedSvelteComponentNames,
-        componentsPath
+        componentsPath,
       );
 
       // Construct script tag content
